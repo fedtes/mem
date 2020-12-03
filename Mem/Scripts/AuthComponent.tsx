5312b42f94b5727d5830744c6d87722cdeba1416
fedtes <federico.tesser@hotmail.it>
2020-12-03 14:21:37 +0100

    auth part 1

---
+++ Mem/Scripts/AuthComponent.tsx
@@ -0,0 +1,46 @@
+﻿import { useContext, createContext, useState } from "react";
+import { login, signout } from './authentication';
+import * as React from "react";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, useHistory, useLocation } from "react-router-dom";
+
+const authContext = createContext<any>(null);
+
+export interface IClaim {
+
+}
+
+function useProvideAuth() {
+    const [claim, setClaim] = useState<IClaim>(null);
+    const _signin = (username: string, password: string) => login(username, password).then(v => setClaim(v))
+
+    return {
+        claim,
+        login: _signin,
+        signout
+    };
+}
+
+export function useAuth() {
+    return useContext(authContext);
+}
+
+type Props = {
+    children: React.ReactNode
+};
+
+export function ProvideAuth({ children }: Props) {
+    const auth = useProvideAuth();
+    return (
+        <authContext.Provider value={auth}>
+            {children}
+        </authContext.Provider>
+    );
+}
+
+
+export function PrivateRoute({ children, ...rest }) {
+    let auth = useAuth();
+    return (
+        <Route {...rest} render={({ location }) => auth.user ? ( children ) : ( <Redirect to={{ pathname: "/login", state: { from: location }}}/> )} />
+    );
+}
--- Mem/Scripts/app.tsx
+++ Mem/Scripts/app.tsx
@@ -1,22 +1,22 @@
 ﻿import * as React from 'react'
 import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Login from './Login';
-import Memos from './Memos';
+import Login from './Pages/Login';
+import Memos from './Pages/Memos';
+import { ProvideAuth, PrivateRoute } from './AuthComponent';
 
 
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
-                <span> hello</span>
+            <ProvideAuth >
                 <Router>
                     <Switch>
                         <Route path="/login"><Login /></Route>
-                        <Route path="/memos"><Memos /></Route>
+                        <PrivateRoute path="/memos"><Memos /></PrivateRoute>
                     </Switch>
                 </Router>
-            </div>
+            </ ProvideAuth>
         );
     };
 }
